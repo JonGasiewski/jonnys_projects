135b42cab6912a7e1945b318dadb9e0ca08561d6
JonGasiewski <jonnygasiewski@gmail.com>
2016-05-25 20:34:34 -0400

    Initial Commit

---
+++ src/com/jonny/SantaService.java
@@ -0,0 +1,43 @@
+package com.jonny;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class SantaService {
+
+	public static void main(String[] args) {
+		
+		ToyQueue toyQueue = new ToyQueue();
+		
+		Elf larry = new Elf("Larry", 10, 5, 20, toyQueue);
+		Elf curly = new Elf("Curly", 6, 4, 6, toyQueue);
+		Elf moe = new Elf("Moe", 5, 6, 10, toyQueue);
+		SantaSleigh santaSleigh = new SantaSleigh(toyQueue);
+		
+		ExecutorService executor = Executors.newCachedThreadPool();
+		
+		// This section is unnecessary, but illustrates how a Callable can be used instead of a Runnable
+		Future<Toy> larryFuture = executor.submit((Callable<Toy>)larry);
+		try {
+			Toy toy = larryFuture.get();
+			System.out.println("\tCreated toy " + toy);
+		} catch (InterruptedException | ExecutionException e1) {
+			e1.printStackTrace();
+		}
+		
+		executor.execute(curly);
+		executor.execute(moe);
+		executor.execute(santaSleigh);
+		
+		larryFuture.cancel(true);
+		if(larryFuture.isDone())
+			executor.execute(larry);
+		
+		
+		executor.shutdown();
+	}
+
+}
---
+++ src/com/jonny/Toy.java
@@ -0,0 +1,28 @@
+package com.jonny;
+
+public class Toy {
+
+	private final int weight;
+	private final int price;
+	
+	public Toy(int toyWeight, int toyPrice) {
+		
+		this.weight = toyWeight;
+		this.price = toyPrice;
+		
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return String.format("Toy[%s lbs, $%s]", weight, price);
+	}
+}
